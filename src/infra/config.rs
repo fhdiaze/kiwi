d884d4cc85ab2353948f54b196095c971ba5eb52
Fredy Diaz <Fredy.Diaz@vocusgroup.co.nz>
2023-01-04 15:50:46 +1300

    feat: add github actions

--- src/infra/config.rs
+++ src/infra/config.rs
@@ -11,7 +11,7 @@ pub struct DbConfig {
 impl Config {
     pub fn new() -> Self {
         let db_config = DbConfig {
-            connection_string: String::from("mongodb+srv://fredyd:beto8712@cluster0.ilsnrak.mongodb.net/?retryWrites=true&w=majority"),
+            connection_string: String::from("mongodb://localhost:C2y6yDjf5%2FR%2Bob0N8A7Cgv30VRDJIWEHLM%2B4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw%2FJw%3D%3D@localhost:10255/admin?ssl=true"),
             db_name: String::from("kiwi"),
             races_collection: String::from("races"),
         };
